f21f4b1ad126ea66f77460ce71da1fa9db075602
Nate Dhaliwal <nathaniel.shaan@gmail.com>
2026-09-07 05:22:38 +0000

    Move button depending on post_menu and post_menu_hidden_items site settings

--- javascripts/discourse/api-initializers/inline-composer-edit-reply.js
+++ javascripts/discourse/api-initializers/inline-composer-edit-reply.js
@@ -3,19 +3,42 @@ import InlineComposer from "../components/inline-composer";
 import InlineComposerEditButton from "../components/inline-composer-edit-button";
 
 export default apiInitializer((api) => {
+  const siteSettings = api.container.lookup("service:site-settings");
   api.registerValueTransformer(
     "post-menu-buttons",
-    ({ value: dag, context: { post, buttonKeys } }) => {
+    ({ value: dag, context: { post, buttonKeys, state } }) => {
       if (post.canEdit) {
-        dag.add("inline-composer-editing", InlineComposerEditButton, {
+        console.log(buttonKeyBefore(siteSettings, buttonKeys, state.collapsed));
+        dag.add("inline-composer", InlineComposerEditButton, {
           post,
-          after: [buttonKeys.COPY_LINK],
-          before: [buttonKeys.SHOW_MORE],
+          after:
+            buttonKeys[
+              buttonKeyBefore(siteSettings, buttonKeys, state.collapsed)
+            ],
         });
-
         dag.delete(buttonKeys.EDIT);
       }
     }
   );
   api.renderInOutlet("post-content-cooked-html", InlineComposer);
 });
+
+function buttonKeyBefore(siteSettings, buttonKeys, collapsed) {
+  const postMenu = siteSettings.post_menu.split("|");
+  const hiddenItems = siteSettings.post_menu_hidden_items.split("|");
+  const beforeName = postMenu[postMenu.indexOf("edit") - 1];
+
+  for (let key in buttonKeys) {
+    if (buttonKeys[key] === beforeName) {
+      if (!collapsed) {
+        return key;
+      }
+
+      if (!hiddenItems.includes(beforeName)) {
+        return key;
+      } else {
+        return "COPY_LINK";
+      }
+    }
+  }
+}
